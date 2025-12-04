def16519d7dacef9179fa4b1033c569895e1a29f
yasaswinireddy18 <yasaswinip718@gmail.com>
2025-12-04 22:59:49 +0530

    feat: add student model and performance timer

---
+++ client/student.h
@@ -0,0 +1,20 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+#include <sstream>
+
+struct Student {
+    int id{};
+    std::string name;
+    int age{};
+    double grade{};
+
+    std::string toCSV() const {
+        std::ostringstream oss;
+        oss << id << "," << name << "," << age << "," << grade;
+        return oss.str();
+    }
+};
+
+#endif // STUDENT_H
